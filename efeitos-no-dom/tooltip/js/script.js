4d02ff8ab92d3e3a94b12de2c4cd7aeb1fa5e31a
Henrique Diniz <henrique.diniz68@yahoo.com.br>
2024-10-22 21:49:42 -0300

    adicionando tooltip e dropdown menu no projeto animais fantasticos

---
+++ efeitos-no-dom/tooltip/js/script.js
@@ -0,0 +1,13 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initTabNav from './modules/navegacao-tab.js';
+import initAccordion from './modules/accordion.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initTabNav();
+initAccordion();
+initModal();
+initTooltip();
